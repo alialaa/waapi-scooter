ada355205f0df84caa023f7582e574672f27f47e
alialaa <hello@alialaa.com>
2024-05-27 12:07:26 +0300

    deceleration

--- scripts/speed.mjs
+++ scripts/speed.mjs
@@ -8,18 +8,44 @@ function handleSpeed() {
   speedText.textContent = speed;
 }
 
+function updatePlaybackRate(rate) {
+  document.getAnimations().forEach((animation) => {
+    if (animation.animationName !== "vibration") {
+      if (rate === 0) {
+        animation.playbackRate = rate;
+      } else {
+        animation.playbackRate += rate;
+      }
+      handleSpeed();
+    }
+  });
+}
+
+let decelerateInterval;
+
 function accelerate() {
+  clearInterval(decelerateInterval);
   if (
     streetAnimation.playbackRate >= MAX_PLAYBACK_RATE ||
     streetAnimation.playState !== "running"
   )
     return;
-  document.getAnimations().forEach((animation) => {
-    if (animation.animationName !== "vibration") {
-      animation.playbackRate += 0.1;
-      handleSpeed();
-    }
-  });
+  updatePlaybackRate(0.1);
+}
+
+function decelerate(rate) {
+  console.log("hhh");
+  if (
+    streetAnimation.updatePlaybackRate === 0 ||
+    streetAnimation.playState !== "running"
+  )
+    return;
+  updatePlaybackRate(-rate);
+
+  if (streetAnimation.playbackRate <= 0.2) {
+    clearInterval(decelerateInterval);
+    updatePlaybackRate(0);
+  }
 }
 
 function handleKeyDown(e) {
@@ -27,10 +53,12 @@ function handleKeyDown(e) {
     accelerate();
   }
   if (e.key === "ArrowLeft") {
+    decelerate(0.5);
   }
 }
 function handleKeyUp(e) {
   if (e.key === "ArrowRight") {
+    decelerateInterval = setInterval(() => decelerate(0.3), 500);
   }
 }
 
